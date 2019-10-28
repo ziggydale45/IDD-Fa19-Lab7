9c1d1e1529225907d35ddfd42e3f278033ed8d1d
ziggydale45 <50380627+ziggydale45@users.noreply.github.com>
2019-10-28 16:33:41 -0400

    Update client.js

--- public/client.js
+++ public/client.js
@@ -43,18 +43,6 @@ socket.on('server-msg', function(msg) {
       document.body.style.backgroundColor = "white";
       console.log("white")
       takePicture();
-      // crazytown
-      gm('/home/pi/IDD-Fa19-Lab7/public')
-      .flip()
-      .magnify()
-      .rotate('green', 45)
-      .blur(7, 3)
-      .crop(300, 300, 150, 130)
-      .edge(3)
-      .write('/path/to/crazy.jpg', function (err) {
-        if (!err) console.log('crazytown has arrived');
-      })
-      
       break;
     case "dark":
       document.body.style.backgroundColor = "black";
